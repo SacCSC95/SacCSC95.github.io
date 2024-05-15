86bfb264ea5565bb81795b8b8366e880ae985c64
Tim OKeefe <113479557+SacCSC95@users.noreply.github.com>
2024-05-14 21:39:32 -0700

    Assignment5 finished.

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,10 +1,27 @@
+//const btn = document.querySelector("button");
+
 function bigger() {
     document.getElementById("Text").style.fontSize = "25px";
 }
 
-function toBold() {
-    document.getElementById("FancyShmancy").style.fontWeight = "bold";
-
-    var x = document.getElementById("FancyShmancy").defaultChecked;
-    document.getElementById("toBold").innerHTML = x;
+function toFancy() {
+    document.getElementById("Text").style.fontWeight = "bold";
+    document.getElementById("Text").style.color = "blue";
+    document.getElementById("Text").style.textDecoration = "underline";
 }
+
+function toNorm() {
+    document.getElementById("Text").style.fontWeight = "normal";
+    document.getElementById("Text").style.color = "black";
+    document.getElementById("Text").style.textDecoration = "none";
+}
+
+function moo() {
+    var t = document.getElementById("Text");
+    //t.value = t.value.toUpperCase();
+    
+    var parts = t.value.split(".");
+    t.value = parts.join(" - Moo");
+}
+
+//btn.addEventListener("click", bigger());
